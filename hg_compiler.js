37109c6b197bbfc9d429ac0371b24a134b8b9f54
GCD+ <127302048+MAJD12358@users.noreply.github.com>
2024-03-10 20:45:14 +0200

    Create hg_compiler.js

---
+++ hg_compiler.js
@@ -0,0 +1,17 @@
+// hg_compiler.js
+
+// Import config file
+const config = require('./config.json');
+
+// Function to compile HG code to HTML
+function compileToHTML(hgCode) {
+    // Implement compilation logic here
+}
+
+// Function to compile HG code to JavaScript
+function compileToJavaScript(hgCode) {
+    // Implement compilation logic here
+}
+
+// Export functions
+module.exports = { compileToHTML, compileToJavaScript };
